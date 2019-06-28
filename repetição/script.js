4e3680b80883b814b6213e59226f5f102eead42f
nobresxe1 <rgs190385@gmail.com>
2019-06-27 22:32:53 -0300

    aprendendo loop

---
+++ repetição/script.js
@@ -0,0 +1,18 @@
+
+var numero = 5
+
+for (var i = 0; i < numero; i++) {
+
+    console.log('Executando o for, pela ' + i + ' vez')
+}
+console.log('Fim')
+
+
+
+
+while (numero < 10) {
+
+    console.log('Número ' + numero)
+    numero++;
+}
+console.log('Fim')
