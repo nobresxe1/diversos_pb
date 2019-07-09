2b61f0709269bddb1f273a62dab12eeaba5f695e
nobresxe1 <rgs190385@gmail.com>
2019-07-08 22:58:19 -0300

    aprendendo funções

---
+++ funções/script.js
@@ -0,0 +1,17 @@
+
+
+
+function média(n1, n2, n3) {
+    var nota01 = n1;
+    var nota02 = n2;
+    var nota03 = n3;
+    var média = (nota01 + nota02 + nota03) / 3;
+    //console.log(média.toFixed(2))
+
+    return média;
+}
+
+var resultado01 = média(7, 9, 10);
+var resultado02 = média(5, 6, 2);
+
+console.log( resultado01.toFixed(01) + " e " + resultado02.toFixed(01))
